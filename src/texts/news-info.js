2d977dfe629f01c5e8b855e945627de06ee32b88
loperntu <loper.ntu@gmail.com>
2022-11-18 11:47:47 +0800

    shukai photo and activities updated

--- src/texts/news-info.js
+++ src/texts/news-info.js
@@ -1,38 +1,45 @@
 const newsInfo = [
-    {
-        "date":"2021.05.20 | 活動",
-        "title":"5/22舉辦兩場活動",
-        "content" :"中文AI知識庫與自然語言處理核心套件研討會、第四屆語言分析黑客松"
-    },
+  {
+    date: "2022.11.12-14 | 活動",
+    title: "本實驗室舉辦 2022 台大多模態語料分析工作坊",
+    content: "https://multimoco.linguistics.ntu.edu.tw/#workshop",
+  },
 
-    {
-        "date":"2021.01.21 | 活動",
-        "title":"曾昱翔博士受邀演講:「深度學習的超級分類器：Bert實作工作坊」",
-        "content" :"2021.01.21於台大國發所302室舉辦",
-        "link":"https://docs.google.com/presentation/d/1aGdG7OcAc-kIGxYqgBOtGeT83avWOA7pAqord7wrlhA/edit#slide=id.p",
-        "linkName":"Hello Bert!"
-    },
+  {
+    date: "2021.05.20 | 活動",
+    title: "5/22舉辦兩場活動",
+    content: "中文AI知識庫與自然語言處理核心套件研討會、第四屆語言分析黑客松",
+  },
 
-    {
-        "date":"2020.11.22 | 活動",
-        "title":"NLP業界學長姊分享會",
-        "content" :"2020.11.24 於台大樂學館305室舉辦"
-    },
+  {
+    date: "2021.01.21 | 活動",
+    title: "曾昱翔博士受邀演講:「深度學習的超級分類器：Bert實作工作坊」",
+    content: "2021.01.21於台大國發所302室舉辦",
+    link: "https://docs.google.com/presentation/d/1aGdG7OcAc-kIGxYqgBOtGeT83avWOA7pAqord7wrlhA/edit#slide=id.p",
+    linkName: "Hello Bert!",
+  },
 
-    {
-        "date":"2020.11.19 | 活動",
-        "title":"歡迎參加「第二屆語料庫程式實務工作坊」",
-        "content" :"我們介紹中文自動化標記、創建語料庫索引、建立自有語料庫、分享語料庫研究相關專案與應用",
-        "link":"https://lopentu.github.io/Hands-on_Corpus_Linguistics/#registration",
-        "linkName": "HOCOR 2020工作坊網頁"
-    },
+  {
+    date: "2020.11.22 | 活動",
+    title: "NLP業界學長姊分享會",
+    content: "2020.11.24 於台大樂學館305室舉辦",
+  },
 
-    {
-        "date":"2020.11.06 | 賀",
-        "title":"林宜儒同學獲得最佳論文！",
-        "content" :"恭喜本實驗室畢業生林宜儒獲得 2020 台灣語言學學會最佳碩士論文獎，並即將在 11/14 的 NCL 頒獎後發表演講！",
-    },
+  {
+    date: "2020.11.19 | 活動",
+    title: "歡迎參加「第二屆語料庫程式實務工作坊」",
+    content:
+      "我們介紹中文自動化標記、創建語料庫索引、建立自有語料庫、分享語料庫研究相關專案與應用",
+    link: "https://lopentu.github.io/Hands-on_Corpus_Linguistics/#registration",
+    linkName: "HOCOR 2020工作坊網頁",
+  },
 
-]
+  {
+    date: "2020.11.06 | 賀",
+    title: "林宜儒同學獲得最佳論文！",
+    content:
+      "恭喜本實驗室畢業生林宜儒獲得 2020 台灣語言學學會最佳碩士論文獎，並即將在 11/14 的 NCL 頒獎後發表演講！",
+  },
+];
 
-export default newsInfo
+export default newsInfo;

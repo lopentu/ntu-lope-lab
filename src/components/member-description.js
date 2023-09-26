fd18f8c51a6c8458947546894b5c88e42647e53b
muoegu <kmicah1794@gmail.com>
2023-09-26 22:50:39 +0800

    update member grade

--- src/components/member-description.js
+++ src/components/member-description.js
@@ -11,7 +11,7 @@ import pin_er from "../images/members/pin-er.jpg";
 import yu_shiang from "../images/members/yu-shiang.png";
 import yulin from "../images/members/yulin.png";
 import mia from "../images/members/mia.jpg";
-import deborah from "../images/members/deborah.jpg"
+import deborah from "../images/members/deborah.jpg";
 import irene from "../images/members/Irene.jpg";
 import joanne from "../images/members/joanne.png";
 import micah from "../images/members/micah.png";
@@ -47,7 +47,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={amber}
-            name="王伯雅｜博三"
+            name="王伯雅｜博四"
             text="計算語言學、語料庫語言學、詞彙語意學、心理語言學、社會語言學、歷史語言學"
             mail="mailto:diff@cmgsh.tp.edu.tw"
             link="https://www.instagram.com/differe94nt/"
@@ -56,7 +56,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={bk}
-            name="古貿昌｜博三"
+            name="古貿昌｜博四"
             text="計算語言學、詞彙語意學、句法學"
             mail="mailto:d08142002@ntu.edu.tw"
           />
@@ -64,7 +64,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={linda}
-            name="陳韋伶｜博二"
+            name="陳韋伶｜博三"
             text="計算語言學、語意學、語言習得、遛狗"
             mail="mailto:d10142007@ntu.edu.tw"
           />
@@ -84,7 +84,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={ben}
-            name="許家誠｜碩四"
+            name="許家誠｜碩五"
             text="計算語言學、南島語語言學、語料庫語言學、聲韻學"
             mail="mailto:r07142006@ntu.edu.tw"
           />
@@ -92,7 +92,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={debby}
-            name="Debby Chang｜碩四"
+            name="Debby Chang｜碩五"
             text="計算語言、詞彙語意學、符號學"
             mail="mailto:r07142005@ntu.edu.tw"
           />
@@ -100,14 +100,14 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={yulin}
-            name="張鈺琳｜碩二"
+            name="張鈺琳｜碩三"
             text="計算語言學、句法學"
           />
         </Col>
         <Col>
           <MemberCard
             imgSrc={amy}
-            name="周昕妤｜碩二"
+            name="周昕妤｜碩三"
             text="計算語言學、語意學、認知語言學"
             mail="mailto:r10142008@ntu.edu.tw"
           />
@@ -115,7 +115,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={joanne}
-            name="紀柔安｜碩一"
+            name="紀柔安｜碩二"
             text="計算語意學、認知語意學、資料科學"
             mail="jajoanne.chi88@gmail.com"
           />
@@ -131,7 +131,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={irene}
-            name="顏巧函｜碩一"
+            name="顏巧函｜碩二"
             text="計算語意學、認知語意學、社會語言學、隱喻"
             mail="mailto:r11142008@ntu.edu.tw"
           />
@@ -139,7 +139,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={micah}
-            name="橘内每歌 Micah｜碩一"
+            name="橘内每歌 Micah｜碩二"
             text="計算語言學、語料庫語言學、語言獲得"
             mail="mailto:r11142010@ntu.edu.tw"
             link="https://github.com/muoegu"
@@ -148,7 +148,7 @@ const MemberDescription = () => {
         <Col>
           <MemberCard
             imgSrc={deborah}
-            name="Deborah Watty｜碩一"
+            name="Deborah Watty｜碩二"
             text="計算語意學"
             mail="mailto:Deborah.watty@gmail.com"
           />
@@ -157,12 +157,10 @@ const MemberDescription = () => {
 
       <h4 style={{ marginBottom: "5vh", marginTop: "5vh" }}>研究助理</h4>
       <Row lg="5" md="4" sm="3" xs="2" style={{ marginBottom: "5vh" }}>
-        <Col>
-        </Col>
+        <Col></Col>
       </Row>
     </div>
   );
 };
-
 
 export default MemberDescription;

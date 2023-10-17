be9e3970872f5ae176e183e97f317f32acb95a19
miaykc <ykcmia@gmail.com>
2023-10-17 18:55:23 +0800

    10/17

--- src/components/member-description.js
+++ src/components/member-description.js
@@ -124,7 +124,7 @@ const MemberDescription = () => {
           <MemberCard
             imgSrc={mia}
             name="葉凱晴｜碩二"
-            text="計算語言學、句法學"
+            text="計算語言學、句法學、語料庫語言學"
             mail="mailto:ykcmia@gmail.com"
           />
         </Col>
